2c19b2921857b21ad7bd02806b2710df539ed67a
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2021-08-07 21:21:30 +0100

    Made default salutation persistant

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,17 @@
 let fillButton = document.getElementById('fillButton');
+let defaultSalutation = document.getElementById('defaultSalutation');
 
 var gender;
-var defaultSalutation;
+var defaultSalutationValue;
+
+let color = '#3aa757';
+
+getSettings(defaultSalutation);
 
 fillButton.addEventListener("click", async () => {
     // for the current tab, inject the "inject.js" file & execute it
     gender = document.querySelector('input[name="gender"]:checked').value;
-    defaultSalutation = document.getElementById('defaultSalutation').value;
+    defaultSalutationValue = document.getElementById('defaultSalutation').value;
 
     // fillButton.addEventListener("click", async () => {
     //     chrome.runtime.sendMessage({gender: gender}, function(response) {
@@ -17,9 +22,27 @@ fillButton.addEventListener("click", async () => {
     chrome.runtime.sendMessage(
         {
             "gender": gender, 
-            "defaultSalutation": defaultSalutation
+            "defaultSalutation": defaultSalutationValue
         }, 
         function(response) {
             console.log(response.farewell);
       });
 });
+
+defaultSalutation.addEventListener('change', async () => {
+    chrome.storage.local.set({"defaultSalutation": defaultSalutation.value}, function() {
+        console.log('Value is set to ' + defaultSalutation.value);
+      });
+    });
+
+function getSettings(defaultSalutation) {
+    chrome.storage.local.get(['defaultSalutation'], function(data) {
+        if (data['defaultSalutation'] === undefined) {
+            chrome.storage.local.set({"defaultSalutation": defaultSalutation.value}, function() {
+                console.log('Value is set to ' + defaultSalutation.value);
+              });
+        }
+        else {defaultSalutation.value = data['defaultSalutation']};
+    });
+}
+  
